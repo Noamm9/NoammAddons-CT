1ec33e0a867ce766b31511ccc657362ab7b73d3e
Noamm <belkunsama@gmail.com>
2024-04-25 23:49:45 +0300

    Update TeamMatesNameTag.js

--- NoammAddons/features/TeamMatesNameTag.js
+++ NoammAddons/features/TeamMatesNameTag.js
@@ -2,14 +2,14 @@
 /// <reference lib="es2015" />
 
 import Settings from "../Settings"
-import { WorldState } from "../../Atomx/skyblock/World"
+import Dungeon from "../../BloomCore/dungeons/Dungeon"
 
 
 let i
 let players
 
 register(`renderWorld`, (pticks) => {
-    if (!Settings.TeammatesNametag && !WorldState.inDungeons()) return
+    if (!Settings.TeammatesNametag && !Dungeon.inDungeon) return
       players = World.getAllPlayers()
       for (i = 0; i < players.length; i++) {
           if (players[i].getName() != Player.getName() && CheckForNPCs(players[i]) && !players[i].getName().includes(" ")) {
@@ -34,10 +34,18 @@ function DrawNames(player, pticks) {
         let currentY = player.getY()
         let currentZ = player.getZ()
 
-        Tessellator.drawString(`§c§l${players[i].getName()} `, 
+        //Black text for Shadow
+        Tessellator.drawString(`§0§l${players[i].getName()} `, 
+        (lastX + (currentX - lastX) * pticks) - 0.031, 
+        (lastY + (currentY - lastY) * pticks) + 2.50 - 0.021, 
+        (lastZ + (currentZ - lastZ) * pticks), 
+        0, false, 1.25, true)
+        
+        // Real text
+        Tessellator.drawString(`§f§l${players[i].getName()} `, 
         lastX + (currentX - lastX) * pticks, 
         (lastY + (currentY - lastY) * pticks) + 2.5, 
         lastZ + (currentZ - lastZ) * pticks, 
-        0, true, 1, true)
+        0, false, 1.25, true)
     }
 }
