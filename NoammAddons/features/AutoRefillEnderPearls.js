fea06d0801704fb8fccf6fdf5ef725bd9fe015ea
Noamm <belkunsama@gmail.com>
2024-05-06 22:37:36 +0300

    removed rat

--- NoammAddons/features/AutoRefillEnderPearls.js
+++ NoammAddons/features/AutoRefillEnderPearls.js
@@ -9,12 +9,13 @@ register("chat", (player, event) => {
 		const inventory = Player.getInventory();
 		const PearlSlot = inventory.indexOf(368);
 		
-		if (PearlSlot !== -1) {
-		  var PearlAmont = String(inventory.getStackInSlot(PearlSlot));
-		  PearlAmont = PearlAmont.substr(0, PearlAmont.indexOf("x"))
-		  if (PearlAmont == 16) return    
-		  ChatLib.command("gfs ender_pearl " + (16 - Number(PearlAmont)));
-		} else {
-		  ChatLib.command("gfs ender_pearl 16");
-		}
-} ).setCriteria("Starting in 1 second.").setContains();
+		if (PearlSlot !== -1 && inventory.getStackInSlot(PearlSlot).getName() == "Ender Pearl") {
+			if (inventory.getStackInSlot(PearlSlot).getName() != "Ender Pearl") return
+		  	var PearlAmont = String(inventory.getStackInSlot(PearlSlot));
+		  	PearlAmont = PearlAmont.substr(0, PearlAmont.indexOf("x"))
+		  	if (PearlAmont == 16) return    
+		  	ChatLib.command("gfs ender_pearl " + (16 - Number(PearlAmont)));
+		
+		} else ChatLib.command("gfs ender_pearl 16");
+		
+} ).setCriteria("Starting in 1 second.")
