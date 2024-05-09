1caf2100c4e15de7a7bdc4468d9409db8ed56ef7
Noamm <belkunsama@gmail.com>
2024-05-09 23:56:43 +0300

    Update BloodDialougeSkip.js

--- NoammAddons/features/BloodDialougeSkip.js
+++ NoammAddons/features/BloodDialougeSkip.js
@@ -4,22 +4,25 @@
 
 import Settings from "../Settings"
 
-register("chat", () => {
+const Chat = register("chat", () => {
     if (!Settings.BloodDialougeSkip) return
-    let StartTime = new Date().getTime()
-    let BloodTimer = 24_000
-    let BloodTitle = new Text(`&a${BloodTimer}`,
+    const StartTime = new Date().getTime()
+    const BloodTimer = 24_000
+    const BloodTitle = new Text(`&a${BloodTimer}`,
         Renderer.screen.getWidth() / 2 - (Renderer.getStringWidth(`${BloodTimer}`)*5) / 2,  // X
         Renderer.screen.getHeight() / 2 - Renderer.screen.getHeight() / 4)                  // Y
         .setShadow(true).setFormatted(true).setScale(5)                                     // Shadow + Scale
         
-    let RenderTrigger = register("renderOverlay", () => {
-        let TimeLeft = ((BloodTimer - (new Date().getTime() - StartTime))/1000).toFixed(2)
+    const RenderTrigger = register("renderOverlay", () => {
+        const TimeLeft = ((BloodTimer - (new Date().getTime() - StartTime))/1000).toFixed(2)
         BloodTitle.setString(TimeLeft > 18 ? `&a${TimeLeft}` : TimeLeft > 10 ? `&e${TimeLeft}` : TimeLeft > 5 ? `&c${TimeLeft}` : `&4${TimeLeft}`)
         BloodTitle.setX(Renderer.screen.getWidth() / 2 - (Renderer.getStringWidth(`${TimeLeft}`)*5) / 2)
         BloodTitle.draw()
-        if (TimeLeft <= 0) RenderTrigger.unregister()
+        if (TimeLeft <= 0) { Chat.register(); RenderTrigger.unregister() }
     }).unregister()
         
     RenderTrigger.register()
+    Chat.unregister()
+    
 }).setCriteria("The BLOOD DOOR has been opened!")
+
