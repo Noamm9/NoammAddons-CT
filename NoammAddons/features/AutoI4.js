5e83456f9ebc0d9bb93942b8f24a58876ce8ef37
Noamm <belkunsama@gmail.com>
2024-05-31 21:59:01 +0300

    Update AutoI4.js

--- NoammAddons/features/AutoI4.js
+++ NoammAddons/features/AutoI4.js
@@ -6,7 +6,6 @@ import Settings from "../Config/Settings";
 import { ModMessage, PlayerUtils } from "../utils"
 
 
-let FourthDevEnabled = true;
 let doneCoords = new Set()
 let lastShot
 
@@ -20,42 +19,35 @@ register("step", () => {
 	    	doneCoords.clear()
 	    	return
 	    }
-
 	    const possible = DevBlocks.filter(coord => !doneCoords.has(coord))
 	    if (!possible.length) return
-    
-
-
+        
+        
+        
 	    const emeraldLocation = possible.find(({ x, y, z }) => World.getBlockAt(x, y, z).type.getID() === 133)
 	    let xdiff = 0.5
-
+        
 	    if (!emeraldLocation) return
-
+        
 	    doneCoords.add(emeraldLocation)
-
+        
 	    if (emeraldLocation.x === 68 || emeraldLocation.x === 66) xdiff = -0.6
 	    else if (emeraldLocation.x === 64) xdiff = 1.3
-    
-
-
-	    let [yaw, pitch] = PlayerUtils.calcYawPitch({ x: emeraldLocation.x + xdiff, y: emeraldLocation.y + 1.1, z: emeraldLocation.z })
-
-
-        if (doneCoords.size === 0) {
-            PlayerUtils.rotateSmoothly(yaw, pitch, 300)
-            setTimeout(() => PlayerUtils.rightClick(), 310)
-            lastShot = Date.now();
-        }
         
-        else if (doneCoords.size !== 0) {
-            PlayerUtils.rotateSmoothly(yaw, pitch, 200)
-            setTimeout(() => PlayerUtils.rightClick(), 210)
-            lastShot = Date.now();
-            setTimeout(() => predictNextTarget(),240)
-            
-        }
-           
+        
+        
+	    let [yaw, pitch] = PlayerUtils.calcYawPitch({ x: emeraldLocation.x + xdiff, y: emeraldLocation.y + 1.1, z: emeraldLocation.z })
+        ModMessage(doneCoords.size)
+        
 
+
+        PlayerUtils.rotateSmoothly(yaw, pitch, 260)
+        setTimeout(() => {
+            PlayerUtils.rightClick();
+            lastShot = Date.now();
+        }, 300);
+
+        
         if (doneCoords.size === 9) {
             setTimeout(() => {
                 ModMessage("&dFinished shooting all blocks!")
@@ -72,16 +64,17 @@ register("worldUnload", () => doneCoords.clear())
 
 
 const DevBlocks  = [
-    { x: 66, y: 128, z: 50 },
-    { x: 66, y: 126, z: 50 },
     { x: 64, y: 126, z: 50 },
-	{ x: 64, y: 128, z: 50 },
-    { x: 68, y: 128, z: 50 },
-    { x: 66, y: 130, z: 50 },
-    { x: 64, y: 130, z: 50 },
+    { x: 66, y: 126, z: 50 },
     { x: 68, y: 126, z: 50 },
+	{ x: 64, y: 128, z: 50 },
+    { x: 66, y: 128, z: 50 },
+    { x: 68, y: 128, z: 50 },
+    { x: 64, y: 130, z: 50 },
+    { x: 66, y: 130, z: 50 },
     { x: 68, y: 130, z: 50 }
-]
+].reverse()
+
 
 
 function isNearPlate () {
@@ -90,45 +83,3 @@ function isNearPlate () {
 } 
 
 
-function predictNextTarget() {
-    const remaining = DevBlocks.filter(coord => !doneCoords.has(coord));
-    if (!remaining.length) return;
-
-    const randomIndex = Math.round(Math.random() * (remaining.length - 1));
-    const nextTarget = remaining[randomIndex]
-    let xdiff = 0.5
-
-
-    if (nextTarget.x === 68 || nextTarget.x === 66) xdiff = -0.6
-    else if (nextTarget.x === 64) xdiff = 1.3
-    
-
-    let [yaw, pitch] = PlayerUtils.calcYawPitch({ x: nextTarget.x + xdiff, y: nextTarget.y + 1.1, z: nextTarget.z })
-    PlayerUtils.rotateSmoothly(yaw, pitch, 200, () => {})
-}
-
-
-
-
-/*
- W.I.P C:
-
-function getBowShootSpeed () {
-    const bow = Player.getInventory().getItems().slice(0, 9).find(a => a?.getID() === 261);
-    if (!bow) return 500
-
-    const lore = bow.getLore();
-
-    let shotSpeed = 300; 
-
-    for (let line of lore) {
-        const match = line.removeFormatting().match(/^Shot Cooldown: (\d+(?:\.\d+)?)s$/);
-        if (match) {
-            shotSpeed = parseFloat(match[1]) * 1000; 
-            break
-        }
-    }
-
-    return shotSpeed
-}
-*/
