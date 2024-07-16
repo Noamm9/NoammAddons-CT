313f28b5354193ce5dff3c69984b5b8ee3e03f84
Noamm <belkunsama@gmail.com>
2024-07-16 23:17:28 +0300

    Update CustomFOV.js

--- NoammAddons/Features/CustomFOV.js
+++ NoammAddons/Features/CustomFOV.js
@@ -6,19 +6,7 @@ import Settings from "../Settings";
 import { registerWhen } from "../utils";
 
 const EntityViewRenderEventFOVModifier = net.minecraftforge.client.event.EntityViewRenderEvent.FOVModifier
-
-
-function StartOrStop() {
-	return Settings.CustomFOV
-}
-
-
 const trigger = register(EntityViewRenderEventFOVModifier, () => Client.settings.setFOV(Settings.FOV))
 
 
-
-registerWhen(trigger, StartOrStop)
-
-
-
-
+registerWhen(trigger, () => Settings.CustomFOV)
